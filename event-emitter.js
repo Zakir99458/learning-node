1c96184a4143de8feed3985a185b3e2189a2f6c9
Mohammad Z. Hossain (Nokia) <mohammad.z.hossain@nokia.com>
2024-05-14 13:37:53 +0300

    event-emitter

--- event-emitter.js
+++ event-emitter.js
@@ -1,1 +1,14 @@
 const EventEmitter = require('events');
+const myEmitter = new EventEmitter();
+
+
+// Creating Emitter
+myEmitter.on('birthday', (name)=>{
+    console.log(`Happy birthday to ${name}`);
+})
+
+myEmitter.on('birthday', () =>{
+    console.log('I will send you gift....');
+})
+
+myEmitter.emit('birthday', 'Zakir')
